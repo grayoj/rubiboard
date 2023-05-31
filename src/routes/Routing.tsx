d4e6209b01b915bca2daf4309f1eaa1c5bccdedf
grayoj <Mgeraldoj07@gmail.com>
2023-05-31 15:20:29 +0100

    Session: Update protected routes.

--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -1,32 +1,33 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Index from '../pages/Index';
-import Login from '../pages/Login';
-import Landing from '../pages/Landing';
-import AuthGateway from '../pages/AuthGateway';
-import SignupForm from '../ui/form/SignupForm';
-import Success from '../pages/Success';
-import { randomRoutePath } from '../hooks/useRandomStringRoute';
-import NotFound from '../pages/NotFound';
-import SetupPage from '../pages/SetupPage';
-import DashboardPage from '../pages/DashboardPage';
-import { SessionProvider } from '../providers/SessionProvider';
-import ProfilePage from '../pages/ProfilePage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Index from "../pages/Index";
+import Login from "../pages/Login";
+import Landing from "../pages/Landing";
+import AuthGateway from "../pages/AuthGateway";
+import SignupForm from "../ui/form/SignupForm";
+import Success from "../pages/Success";
+import { randomRoutePath } from "../hooks/useRandomStringRoute";
+import NotFound from "../pages/NotFound";
+import SetupPage from "../pages/SetupPage";
+import DashboardPage from "../pages/DashboardPage";
+import { SessionProvider } from "../providers/SessionProvider";
+import ProfilePage from "../pages/ProfilePage";
 
 const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Index />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/landing' element={<Landing />} />
-        <Route path='/auth' element={<AuthGateway />} />
-        <Route path='/auth/signup' element={<SignupForm />} />
-        <Route path='/success' element={<Success />} />
+        <SessionProvider>
+          <Route path="/" element={<Index />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/account" element={<ProfilePage />} />
+          <Route path="*" element={<NotFound />} />
+        </SessionProvider>
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/auth" element={<AuthGateway />} />
+        <Route path="/auth/signup" element={<SignupForm />} />
         <Route path={`/success/${randomRoutePath}`} element={<Success />} />
-        <Route path='/setup' element={<SetupPage />} />
-        <Route path='/dashboard' element={<DashboardPage />} />
-        <Route path='/account' element={<ProfilePage />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/setup" element={<SetupPage />} />
       </Routes>
     </BrowserRouter>
   );
