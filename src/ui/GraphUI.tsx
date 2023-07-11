7a6fd37db47f1b3c0c0497760e28ef7b91ab2cf1
grayoj <Mgeraldoj07@gmail.com>
2023-07-11 17:47:41 +0100

    Graphs: (Rechart): refactor graph UI

--- src/ui/GraphUI.tsx
+++ src/ui/GraphUI.tsx
@@ -1,17 +1,13 @@
 import { GraphComponent } from './graphs/GraphChart';
 
 const GraphUI: React.FC = () => {
-  const deliveryData = [0, 0, 0, 0, 0];
   return (
     <div className='mt-4 bg-darkTheme rounded-md'>
       <h1 className='text-xl font-bold mb-4 text-white py-4 pl-4'>
         Delivery Statistics
       </h1>
 
-      <GraphComponent
-        //@ts-ignore
-        data={deliveryData}
-      />
+      <GraphComponent />
     </div>
   );
 };
