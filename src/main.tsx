d030a8f1d8cea7ab6bc9716acd6cb58c53fc434c
grayoj <Mgeraldoj07@gmail.com>
2023-07-11 17:39:03 +0100

    React Query: Wrap provider around App.

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,17 @@ import './index.css';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import store from './store/store.ts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    <ToastContainer />
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ToastContainer />
+    </QueryClientProvider>
   </React.StrictMode>
 );
