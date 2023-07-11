ad0fedaf9c7cd1a5435abc2eafe89a3af20f59aa
grayoj <Mgeraldoj07@gmail.com>
2023-07-11 17:43:46 +0100

    Interafces: Build out delivery interfaces.

--- src/types/DeliveryInterfaces.ts
+++ src/types/DeliveryInterfaces.ts
@@ -12,9 +12,34 @@ export interface Delivery {
  delivered: boolean;
  rider: string
  status: string;
+ deliveryRider: string;
+ deliveryTime: string;
+}
+
+export interface Payments {
+ id: number;
+ customerName: string;
+ customerEmail: string;
+ customerNumber: string;
+ dropAddress: string;
+ pickupAddress: string;
+ packageName: string;
+ packageType: string;
+ amount: number;
+ timestamp: string;
+ delivered: boolean;
+ rider: string
+ status: string;
+ referenceCode: string;
 }
 
 export interface DeliveryResponse {
+ totalElements: number;
  content: Delivery[];
+ totalPages: number;
+}
+
+export interface PaymentResponse {
+ content: Payments[];
  totalPages: number;
 }
