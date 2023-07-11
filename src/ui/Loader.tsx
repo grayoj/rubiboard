56db55b30f0917a4569d490e2fe8cb0632b240bd
grayoj <Mgeraldoj07@gmail.com>
2023-07-11 17:44:34 +0100

    Style: Updated loading states.

--- src/ui/Loader.tsx
+++ src/ui/Loader.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { CircleLoader } from 'react-spinners';
+import { PropagateLoader } from 'react-spinners';
 
 interface LoaderProps {
   className?: string;
 }
 
 const Loader: React.FC<LoaderProps> = ({}) => {
-  return <CircleLoader color='#97E398' size={20} />;
+  return <PropagateLoader color='#97E398' size={20} />;
 };
 
 export default Loader;
--- src/ui/Preloader.tsx
+++ src/ui/Preloader.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BounceLoader } from 'react-spinners';
+import { PropagateLoader } from 'react-spinners';
 
 const PreLoader: React.FC = () => {
   return (
     <div className='flex justify-center items-center h-screen'>
-      <BounceLoader color='#97E398' size={50} />
+      <PropagateLoader color='#97E398' size={50} />
     </div>
   );
 };
